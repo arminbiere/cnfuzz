544dc3954de7b2e6059890f5ce37986c90a7cb84
Armin Biere <biere@jku.at>
2009-05-22 17:02:14 +0200

    pick working

--- cnfuzz.c
+++ cnfuzz.c
@@ -1,8 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <sys/times.h>
+#include <unistd.h>
+
+static int seed;
+static int layers;
+
+static int
+pick (int from, int to)
+{
+  assert (from <= to);
+  return (rand() % (to - from + 1)) + from;
+}
 
 int
-main (void)
+main (int argc, char ** argv)
 {
+  seed = (argc > 1) ? atoi (argv[1]) : abs ((times(0) * getpid ()) >> 1);
+  printf ("c seed %d\n", seed);
+  srand (seed);
+  layers = pick (1, 10);
+  printf ("c layers %d\n", layers);
+  return 0;
 }
